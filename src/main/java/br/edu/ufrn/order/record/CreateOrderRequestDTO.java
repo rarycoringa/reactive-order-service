cb27d60e70ea2e90750b98e3650775e30fb38b2f
Rary Coringa <rary.goncalves.123@ufrn.edu.br>
2025-12-06 21:53:16 -0300

    add json property annotations

--- src/main/java/br/edu/ufrn/order/record/CreateOrderRequestDTO.java
+++ src/main/java/br/edu/ufrn/order/record/CreateOrderRequestDTO.java
@@ -1,9 +1,11 @@
 package br.edu.ufrn.order.record;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record CreateOrderRequestDTO(
-    String productId,
-    Integer productQuantity,
-    Integer splitInto,
-    String cardNumber,
+    @JsonProperty("product_id") String productId,
+    @JsonProperty("product_quantity") Integer productQuantity,
+    @JsonProperty("split_into") Integer splitInto,
+    @JsonProperty("card_number") String cardNumber,
     String address
 ) {}
--- src/main/java/br/edu/ufrn/order/record/OrderResponseDTO.java
+++ src/main/java/br/edu/ufrn/order/record/OrderResponseDTO.java
@@ -1,13 +1,16 @@
-package br.edu.ufrn.order.controller;
+package br.edu.ufrn.order.record;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record OrderResponseDTO(
     String id,
-    String productId,
-    Integer productQuantity,
-    String paymentChargeId,
-    String paymentRefundId,
-    String shippingId,
-    Instant createdAt
+    @JsonProperty("product_id") String productId,
+    @JsonProperty("product_quantity") Integer productQuantity,
+    @JsonProperty("payment_charge_id") String paymentChargeId,
+    @JsonProperty("payment_refund_id") String paymentRefundId,
+    @JsonProperty("shipping_id") String shippingId,
+    @JsonProperty("created_at") @JsonFormat(shape = JsonFormat.Shape.STRING) Instant createdAt
 ) {}
